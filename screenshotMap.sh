a0777dfce41a3647cefacc69bca1d02c33921fd8
Georgia Miskell <georgia.miskell@gmail.com>
2018-03-29 09:04:58 +1300

    tidy script

--- screenshotMap.sh
+++ screenshotMap.sh
@@ -1,18 +1,9 @@
-# this script allows the user to set up an automatic screenshot of a webpage, using cron
-
-
-
 # find path
-#echo(PATH)
+echo(PATH)
 
 #!/bin/bash
-
-#SHELL=/bin/sh
-#PATH=/home/analyser/Dropbox:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
-
-#PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 export DISPLAY=:0.0
 
 echo 'start'
@@ -25,17 +16,16 @@ file_type='screenshot'
 filename=$file_type$DATE
 
 # open webpage
-
 firefox 'https://www.google.co.nz/maps/@-36.8730671,174.7501478,15z/data=!5m1!1e1' &
 declare -i PID=$!
 
 sleep $network_time
 
-# get screenshot
-
+# get screenshot using scrot
 scrot $filename.png 
 
 sleep $network_time
 
+# close browser
 kill ${PID}
 
